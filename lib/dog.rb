b92d711f4516e6177ccca22ee8bb4b72e116916d
“zed” <zed@kolk.me>
2021-01-12 15:48:17 -0600

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,22 @@
+class Dog
+    def initialize name=nil, breed=nil
+        @name = name
+        @breed = breed
+    end
+    
+    def name=name
+        @name = name
+    end
+        
+    def name
+        @name
+    end
+        
+    def breed=breed
+        @breed = breed
+    end
+        
+    def breed
+     @breed
+    end
+end
